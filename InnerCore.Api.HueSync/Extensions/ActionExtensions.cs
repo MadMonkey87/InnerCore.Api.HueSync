81ae760db945dd18751b36b5ec47b16589ab8727
MadMonkey87 <myself@philippewechsler.ch>
2020-01-17 20:47:27 +0100

    extensions

---
+++ InnerCore.Api.HueSync/Extensions/ActionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using InnerCore.Api.HueSync.Models;
+using System;
+
+namespace InnerCore.Api.HueSync.Extensions
+{
+	public static class ActionExtensions
+	{
+		public static ActionCommand SetMode(this ActionCommand action, Mode mode)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			action.Mode = mode;
+			return action;
+		}
+
+		public static ActionCommand SetIntensity(this ActionCommand action, Intensity intensity)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			action.Intensity = intensity;
+			return action;
+		}
+
+		public static ActionCommand SetBrightness(this ActionCommand action, int brightness)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			action.Brightness = brightness;
+			return action;
+		}
+
+		public static ActionCommand SetHdmiSource(this ActionCommand action, HdmiSource source)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			action.HdmiSource = source;
+			return action;
+		}
+		// todo: nested input actions
+	}
+}
---
+++ InnerCore.Api.HueSync/Extensions/BehaviourExtensions.cs
@@ -0,0 +1,65 @@
+﻿using InnerCore.Api.HueSync.Models;
+using System;
+
+namespace InnerCore.Api.HueSync.Extensions
+{
+	public static class BehaviourExtensions
+	{
+		public static Behaviour SetInactivePowerSave(this Behaviour behaviour, bool enable)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+
+			behaviour.InactivePowerSave = enable ? 0 : 1;
+			return behaviour;
+		}
+
+		public static Behaviour SetCecPowerSave(this Behaviour behaviour, bool enable)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+
+			behaviour.CecPowerSave = enable ? 0 : 1;
+			return behaviour;
+		}
+
+		public static Behaviour SetUsbPowerSave(this Behaviour behaviour, bool enable)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+
+			behaviour.UsbPowerSave = enable ? 0 : 1;
+			return behaviour;
+		}
+
+		public static Behaviour SetHpdInputSwitch(this Behaviour behaviour, bool enable)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+
+			behaviour.HpdInputSwitch = enable ? 0 : 1;
+			return behaviour;
+		}
+
+		public static Behaviour SetArcBypass(this Behaviour behaviour, bool enable)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+
+			behaviour.ArcBypassMode = enable ? 0 : 1;
+			return behaviour;
+		}
+
+		// todo: nested input behaviours
+	}
+}
