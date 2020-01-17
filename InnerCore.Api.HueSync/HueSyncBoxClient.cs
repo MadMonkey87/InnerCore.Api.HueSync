c54aef401a7b3b667ad61fb2c6aac15fa0a08c78
MadMonkey87 <myself@philippewechsler.ch>
2020-01-17 20:46:53 +0100

    basic client

---
+++ InnerCore.Api.HueSync/HueSyncBoxClient.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using InnerCore.Api.HueSync.Models;
+using Newtonsoft.Json;
+
+namespace InnerCore.Api.HueSync
+{
+	// todo: extend s.t the hue bridge details can be changed too
+	// todo: allow to rename the hdmi inputsA
+	public class HueSyncBoxClient
+	{
+		private string _accessToken;
+
+		private readonly string _apiBase;
+
+		private HttpClient _httpClient;
+
+		private JsonSerializerSettings _serializerSettings = 
+			new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+
+		public HueSyncBoxClient(string ip)
+		{
+			if (ip == null)
+				throw new ArgumentNullException(nameof(ip));
+
+			_apiBase = $"https://{ip}";
+		}
+
+		public void Initialize(string accessToken)
+		{
+			_accessToken = accessToken ?? throw new ArgumentNullException(nameof(accessToken));
+		}
+
+		/// <summary>
+		/// Retrieves an access token from the box. This needs to be called first to initiate the registration progress and will return null. The user then has to
+		/// press and hold the physical button on the box until the led turns green. A further registration attempt then will return the access token which should be kept
+		/// safe for further authorization
+		/// </summary>
+		/// <param name="applicationName">any application name</param>
+		/// <param name="applicationSecret">it is not yet known how and where to register an application, but MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI= seems to work fine</param>
+		/// <param name="clientName">any client name</param>
+		/// <returns>null if the user needs to press the physical button on the box or the access token if the registration was successful</returns>
+		public async Task<string> RegisterAsync(string applicationName, string applicationSecret, string clientName)
+		{
+			if (applicationName == null)
+				throw new ArgumentNullException(nameof(applicationName));
+			if (applicationSecret == null)
+				throw new ArgumentNullException(nameof(applicationSecret));
+			if (clientName == null)
+				throw new ArgumentNullException(nameof(clientName));
+
+			var request = new RegistrationRequest() { ApplicationName = applicationName, ApplicationSecret = applicationSecret, ClientName = clientName };
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.PostAsync(new Uri($"{_apiBase}/api/v1/registrations"), SerializeRequest(request)).ConfigureAwait(false);
+
+			var registrationResponse = await HandleResponseAsync<RegistrationResponse>(response);
+
+			Initialize(registrationResponse.AccessToken);
+
+			return registrationResponse.Success ? registrationResponse.AccessToken : null;
+		}
+
+		public async Task<Device> GetDeviceAsync()
+		{
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.GetAsync(new Uri($"{_apiBase}/api/v1")).ConfigureAwait(false);
+			return await HandleResponseAsync<Device>(response);
+		}
+
+		public async Task<State> GetStateAsync()
+		{
+			CheckInitialized();
+
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.GetAsync(new Uri($"{_apiBase}/api/v1")).ConfigureAwait(false);
+			return await HandleResponseAsync<State>(response);
+		}
+
+		public async Task PerformActionAsync(ActionCommand action)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+			CheckInitialized();
+
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.PutAsync(new Uri($"{_apiBase}/api/v1/execution"), SerializeRequest(action)).ConfigureAwait(false);
+			await HandleResponseAsync(response);
+		}
+
+		public async Task ApplyBehaviourAsync(Behaviour behaviour)
+		{
+			if (behaviour == null)
+			{
+				throw new ArgumentNullException(nameof(behaviour));
+			}
+			CheckInitialized();
+
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.PutAsync(new Uri($"{_apiBase}/api/v1/behaviour"), SerializeRequest(behaviour)).ConfigureAwait(false);
+			await HandleResponseAsync(response);
+		}
+
+		public async Task PerformDeviceActionAsync(DeviceAction action)
+		{
+			CheckInitialized();
+
+			var client = await GetHttpClient().ConfigureAwait(false);
+			var response = await client.PutAsync(new Uri($"{_apiBase}/api/v1/behaviour"), SerializeRequest(new DeviceCommand() { Action = action })).ConfigureAwait(false);
+			await HandleResponseAsync(response);
+		}
+
+		private HttpContent SerializeRequest(Object request)
+		{
+			return new StringContent(JsonConvert.SerializeObject(request, _serializerSettings), Encoding.UTF8, "application/json");
+		}
+
+		private async Task HandleResponseAsync(HttpResponseMessage response)
+		{
+			if (!response.IsSuccessStatusCode)
+			{
+				var error = JsonConvert.DeserializeObject<GenericError>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+				throw new HueSyncBoxException(error);
+			}
+		}
+
+		private async Task<T> HandleResponseAsync<T>(HttpResponseMessage response)
+		{
+			await HandleResponseAsync(response);
+
+			return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+		}
+
+		private Task<HttpClient> GetHttpClient()
+		{
+			// return per-thread HttpClient
+			if (_httpClient == null)
+			{
+				_httpClient = new HttpClient();
+				_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+			}
+
+			return Task.FromResult(_httpClient);
+		}
+
+		private void CheckInitialized()
+		{
+			if (string.IsNullOrEmpty(_accessToken))
+				throw new InvalidOperationException("HueSyncBoxClient is not initialized. First call RegisterAsync or Initialize.");
+		}
+	}
+}
---
+++ InnerCore.Api.HueSync/HueSyncBoxException.cs
@@ -0,0 +1,10 @@
+﻿using InnerCore.Api.HueSync.Models;
+using System;
+
+namespace InnerCore.Api.HueSync
+{
+	public class HueSyncBoxException : Exception
+	{
+		public HueSyncBoxException(GenericError error) : base($"the hue sync box responded with {error.Code}, '{error.Message}'") { }
+	}
+}
