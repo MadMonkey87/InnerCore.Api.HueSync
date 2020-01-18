eb17ce6d437cb1eb49e588c2d9c11cc03e0dbfd8
MadMonkey87 <myself@philippewechsler.ch>
2020-01-18 11:52:31 +0100

    input status completed

--- InnerCore.Api.HueSync/Models/InputStatus.cs
+++ InnerCore.Api.HueSync/Models/InputStatus.cs
@@ -2,13 +2,15 @@
 
 namespace InnerCore.Api.HueSync.Models
 {
-	// todo: check if there are further values possible
 	public enum InputStatus
 	{
 		[EnumMember(Value = "unplugged")]
 		Unplugged,
 
 		[EnumMember(Value = "plugged")]
-		Plugged
+		Plugged,
+
+		[EnumMember(Value = "linked")]
+		Linked
 	}
 }
