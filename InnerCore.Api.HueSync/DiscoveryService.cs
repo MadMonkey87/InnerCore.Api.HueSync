c133c11ecfb5da2ab249e6941869e8d5709642e9
MadMonkey87 <myself@philippewechsler.ch>
2020-01-26 00:49:30 +0100

    discovery client

---
+++ InnerCore.Api.HueSync/DiscoveryService.cs
@@ -0,0 +1,60 @@
+﻿using InnerCore.Api.HueSync.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using System.Threading.Tasks;
+using InnerCore.Api.HueSync.Extensions;
+
+namespace InnerCore.Api.HueSync
+{
+	public static class DiscoveryService
+	{
+        private const int port = 8888;
+        private const string discoveryRequest = "{\"operation\": null}";
+
+		public static async Task<IEnumerable<DiscoveryResult>> Discover(CancellationToken cancellationToken)
+		{
+			var result = new List<DiscoveryResult>();
+
+            using(var client = new UdpClient(port))
+            {
+                try
+                {
+                    var discoveryMessage = Encoding.ASCII.GetBytes(discoveryRequest);
+                    await client
+                        .SendAsync(discoveryMessage, discoveryMessage.Length, new IPEndPoint(IPAddress.Broadcast, port))
+                        .WithCancellation(cancellationToken);
+
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        var receiveResult = await client.ReceiveAsync().WithCancellation(cancellationToken);
+                        var clientRequest = Encoding.ASCII.GetString(receiveResult.Buffer);
+
+                        try
+                        {
+                            var discoveryResult = JsonConvert.DeserializeObject<DiscoveryResult>(clientRequest);
+                            discoveryResult.IpAddress = receiveResult.RemoteEndPoint.Address.ToString();
+                            result.Add(discoveryResult);
+                        }
+                        catch (JsonSerializationException ex)
+                        {
+                            // at that point we received an invalid message, usually this is just our own discovery message or someone other than a hue sync box
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    client.Close();
+                }
+            }
+
+            return result.ToArray();
+		}
+    }
+}
