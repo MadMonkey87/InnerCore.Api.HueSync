8ea4c363d035dbe4c4e20307bcfc26833e1917ae
MadMonkey87 <myself@philippewechsler.ch>
2020-01-25 23:54:00 +0100

    discovery result

---
+++ InnerCore.Api.HueSync/Models/DiscoveryResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace InnerCore.Api.HueSync.Models
+{
+	[DataContract]
+	public class DiscoveryResult
+	{
+		[DataMember(Name = "operation")]
+		public int Operation { get; set; }
+
+		[DataMember(Name = "name")]
+		public string Name { get; set; }
+
+		[DataMember(Name = "deviceType")]
+		public string DeviceType { get; set; }
+
+		[DataMember(Name = "uniqueId")]
+		public string UniqueId { get; set; }
+
+		[DataMember(Name = "ipAddress")]
+		public string IpAddress { get; set; }
+	}
+}
