9d4b8d9c35f2701d552c0041d54d2f550aa62dbb
MadMonkey87 <myself@philippewechsler.ch>
2020-01-26 01:14:18 +0100

    discovery demo

--- InnerCore.Api.HueSync.Sample/Program.cs
+++ InnerCore.Api.HueSync.Sample/Program.cs
@@ -2,6 +2,7 @@
 using InnerCore.Api.HueSync.Models.Command;
 using InnerCore.Api.HueSync.Models.Enum;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace InnerCore.Api.HueSync.Sample
@@ -10,14 +11,33 @@ namespace InnerCore.Api.HueSync.Sample
 	{
 		static async Task Main(string[] args)
 		{
-			Console.WriteLine("please enter the ip address of your Hue Sync Box:");
-			var ip = Console.ReadLine();
+			// 1. discover device(s)
+			// =====================
 
+			Console.WriteLine("searching for hue sync boxes in the local network");
+			var discoveryResults = await DiscoveryService.Discover(TimeSpan.FromSeconds(3));
+			if (!discoveryResults.Any())
+			{
+				Console.WriteLine("no device could be found in the local network");
+				Console.ReadLine();
+				return;
+			}
+
+			Console.WriteLine($"found {discoveryResults.Count()} device(s) -> the first one will be used for this demo");
+			foreach (var discoveryResult in discoveryResults)
+			{
+				Console.WriteLine($"{discoveryResult.Name} ({discoveryResult.IpAddress})");
+			}
+
+			// 2. retrieve access token
+			// ========================
+
+			var ip = discoveryResults.First().IpAddress;
 			var client = new HueSyncBoxClient(ip);
 
 			// you can validate the connection by calling for device details (no access token needed yet)
 			var device = await client.GetDeviceAsync();
-			Console.WriteLine($"found the device '{device.Name}'");
+			Console.WriteLine($"connected to {device.Name}");
 
 			Console.WriteLine("if you already have an access token you can enter it now or press enter to start the registration progress");
 			string accessToken = Console.ReadLine();
@@ -38,12 +58,18 @@ namespace InnerCore.Api.HueSync.Sample
 				// here you would usually persist the token and for the next app start you would call client.Initialize(accessToken) directly instead of registering
 			}
 
+			// 3. read device details and state
+			// ================================
+
 			var state = await client.GetStateAsync();
 			Console.WriteLine("Here are some details about your Hue Sync Box:");
 			Console.WriteLine($"firmware: {state.Device.FirmwareVersion}");
 			Console.WriteLine($"current mode: {state.Execution.Mode}");
 			Console.WriteLine($"current input: {state.Execution.HdmiSource}");
 			Console.WriteLine($"current content: {state.Hdmi.ContentSpecs}");
+
+			// 4. perform some actions on the device
+			// =====================================
 
 			Console.WriteLine("press enter to set the mode to 'game, intense', apply the max brightness and change the source to hdmi 2");
 			Console.ReadLine();
@@ -53,9 +79,14 @@ namespace InnerCore.Api.HueSync.Sample
 				.SetHdmiSource(HdmiSource.Input2);
 			await client.ApplyExecutionCommandAsync(action);
 
+			Console.WriteLine("press enter start syncing");
+			Console.ReadLine();
+			action = new ExecutionCommand().SetSyncActive(true);
+			await client.ApplyExecutionCommandAsync(action);
+
 			Console.WriteLine("press enter stop syncing");
 			Console.ReadLine();
-			action = new ExecutionCommand().SetMode(Mode.Passthrough);
+			action = new ExecutionCommand().SetSyncActive(false);
 			await client.ApplyExecutionCommandAsync(action);
 
 			Console.WriteLine("press enter put the box into standby mode");
